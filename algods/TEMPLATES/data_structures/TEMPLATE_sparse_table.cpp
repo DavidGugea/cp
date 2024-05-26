2d516bdb0e3593c4c6081c1a0219f1f971ad893f
David Gugea <davidgugea@yahoo.com>
2024-05-26 14:18:24 +0200

    algods - template - data structures - sparse table - fix bug

--- algods/TEMPLATES/data_structures/TEMPLATE_sparse_table.cpp
+++ algods/TEMPLATES/data_structures/TEMPLATE_sparse_table.cpp
@@ -25,7 +25,7 @@ int main() {
     }
 
     // construct the table
-    for (int i = 1; i < n; ++i)
+    for (int i = 1; i < dpr; ++i)
         for (int j = 0; (j + (1 << i)) <= n; ++j)
             dp[i][j] = min(dp[i-1][j], dp[i-1][j+1<<(i-1)]);
 
