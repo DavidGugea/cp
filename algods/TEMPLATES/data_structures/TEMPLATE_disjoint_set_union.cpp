571c92cabe1cb616b22b8963e897bdaa20bd525b
David Gugea <davidgugea@yahoo.com>
2025-08-08 12:22:49 +0200

    change TEMPLATE_disjoin_set_union.cpp

--- algods/TEMPLATES/data_structures/TEMPLATE_disjoint_set_union.cpp
+++ algods/TEMPLATES/data_structures/TEMPLATE_disjoint_set_union.cpp
@@ -7,21 +7,21 @@ public:
     unordered_map<int, int> size;
 
     void make_set(int v) {
+        if (parent.find(v) != parent.end())
+            return;
+
         parent[v] = v;
         size[v] = 1;
     }
 
     int find_set(int v) {
-        // path compression
         if (v == parent[v])
             return v;
 
-        size[parent[v]] -= 1;
         return parent[v] = find_set(parent[v]);
     }
 
     void union_sets(int a, int b) {
-        // union by size => a > b, attach b to a
         a = find_set(a);
         b = find_set(b);
 
